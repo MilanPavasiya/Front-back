c0161d4885ce22a5cfa1fa6b734cabaa1f472e98
MilanPavasiya <milpavasiya@gmail.com>
2025-11-30 07:09:05 -0500

    Data Modelling

---
+++ backend/models/todos/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+		},
+		password: {
+			type: String,
+			required: [true, 'Please provide a password'],
+		},
+	},
+	{ timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
