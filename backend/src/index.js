3683326a49f127123d0a1fa8d6c64b30d394e649
MilanPavasiya <milpavasiya@gmail.com>
2025-12-15 23:15:53 -0500

    intial routing and controller setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,22 @@ import dotenv from "dotenv";
 import connectDb from "./database/index.js";
 import { app } from "./app.js";
 
-dotenv.config({
-  path: "./env",
-});
+dotenv.config();
 
-connectDb()
-  .then(() => app.listen(process.env.PORT || 8000, () => console.log(`server is running on port : ${process.env.PORT}`))
-  .catch((e) => {
-    console.log("MongoDB connection failed", e);
-  })
+const startServer = async () => {
+  try {
+    await connectDb();
 
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`server is running on port : ${process.env.PORT} `)
+    );
+  } catch (error) {
+    console.log("Error starting server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
 /*
 import express from "express";
 const app = express();
